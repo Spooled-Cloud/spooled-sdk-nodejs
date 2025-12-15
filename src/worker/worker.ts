f3009da5a5c2303db8b9659358ab9e26fe0a5664
Yevhen Salitrynskyi <ysalitrynskyi@gmail.com>
2025-12-14 22:09:12 -0500

    chore(release): v1.0.5

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -75,7 +75,7 @@ export class SpooledWorker {
       ...DEFAULT_OPTIONS,
       hostname: osHostname(),
       workerType: 'nodejs',
-      version: '1.0.4',
+      version: '1.0.5',
       metadata: {},
       ...options,
     } as Required<SpooledWorkerOptions>;
