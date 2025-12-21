f1fbf59f2a0c3e6e661e9d6440f5de94e3a57f26
Yevhen Salitrynskyi <ysalitrynskyi@gmail.com>
2025-12-21 12:06:06 -0500

    chore(release): v1.0.26

--- src/config.ts
+++ src/config.ts
@@ -111,12 +111,12 @@ export const DEFAULT_CONFIG = {
     successThreshold: 3,
     timeout: 30000,
   } satisfies CircuitBreakerConfig,
-  userAgent: '@spooled/sdk-nodejs/1.0.25',
+  userAgent: '@spooled/sdk-nodejs/1.0.26',
   autoRefreshToken: true,
 } as const;
 
 /** SDK version */
-export const SDK_VERSION = '1.0.25';
+export const SDK_VERSION = '1.0.26';
 
 /** API version prefix */
 export const API_VERSION = 'v1';
--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -75,7 +75,7 @@ export class SpooledWorker {
       ...DEFAULT_OPTIONS,
       hostname: osHostname(),
       workerType: 'nodejs',
-      version: '1.0.25',
+      version: '1.0.26',
       metadata: {},
       ...options,
     } as Required<SpooledWorkerOptions>;
